e2dd2fde7e67c751ca5141ec7ecb257b8863b97c
mohseen <msaudagar@gammastack.com>
2023-03-14 17:50:45 +0530

    mend

--- Musico/config/routes.rb
+++ Musico/config/routes.rb
@@ -7,5 +7,11 @@ Rails.application.routes.draw do
   post 'login' => 'user#validate'
   get 'logout' => 'user#logout'
   
+
+  get  'artist/signup' => 'artist#new' 
+  post 'artist/signup' => 'artist#create' 
+  get 'artist/login' => 'artist#login'
+  post 'artist/login' => 'artist#validate'
+  get 'artist/logout' => 'artist#logout'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- Musico/test/controllers/artist_controller_test.rb
+++ Musico/test/controllers/artist_controller_test.rb
@@ -1,23 +1,33 @@
 require "test_helper"
 
 class ArtistControllerTest < ActionDispatch::IntegrationTest
-  test "should get name" do
-    get artist_name_url
+  test "should get new" do
+    get artist_new_url
     assert_response :success
   end
 
-  test "should get email" do
-    get artist_email_url
+  test "should get create" do
+    get artist_create_url
     assert_response :success
   end
 
-  test "should get phone_no" do
-    get artist_phone_no_url
+  test "should get destroy" do
+    get artist_destroy_url
     assert_response :success
   end
 
-  test "should get password" do
-    get artist_password_url
+  test "should get login" do
+    get artist_login_url
+    assert_response :success
+  end
+
+  test "should get validate" do
+    get artist_validate_url
+    assert_response :success
+  end
+
+  test "should get logout" do
+    get artist_logout_url
     assert_response :success
   end
 end
